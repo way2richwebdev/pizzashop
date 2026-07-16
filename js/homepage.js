fdb2f99c4cca1cb6453928795e9e704ea223c44b
markwebdev <markwebdev@mail.ru>
2026-07-16 20:47:39 +0300

    filter added and working correct

--- js/homepage.js
+++ js/homepage.js
@@ -11,12 +11,38 @@ window.addEventListener('DOMContentLoaded', () => {
         },
     });
 
+    const filterItems = document.querySelectorAll('.menu-filter__item');
+
+    const filterItemClickHandler = (target) => {
+        document.querySelector('.menu-filter__item.active').classList.remove('active');
+
+        target.classList.add('active');
+    };
+
     const menu = [
-        {id: 1, preview: './assets/img/menu1.png', label: 'Итальянская', description: 'Лук, картофель, помидоры, грибы, сыр, оливки, мясо...', sizes: [22, 28, 33], price: 350},
-        {id: 2, preview: './assets/img/menu2.png', label: 'Венеция', description: 'Лук, картофель, помидоры, грибы, сыр, оливки, мясо...', sizes: [22, 28, 33], price: 380},
-        {id: 3, preview: './assets/img/menu3.png', label: 'Мясная', description: 'Лук, картофель, помидоры, грибы, сыр, оливки, мясо...', sizes: [22, 28, 33], price: 400},
-        {id: 4, preview: './assets/img/menu4.png', label: 'Сырная', description: 'Лук, картофель, помидоры, грибы, сыр, оливки, мясо...', sizes: [22, 28, 33], price: 330},
+        {id: 1, preview: './assets/img/menu1.png', categories: ['Мясная', 'Грибы'], label: 'Итальянская', description: 'Лук, картофель, помидоры, грибы, сыр, оливки, мясо...', sizes: [22, 28, 33], price: 350},
+        {id: 2, preview: './assets/img/menu2.png', categories: ['Морепродукты'], label: 'Венеция', description: 'Лук, картофель, помидоры, грибы, сыр, оливки, мясо...', sizes: [22, 28, 33], price: 380},
+        {id: 3, preview: './assets/img/menu3.png', categories: ['Мясная'], label: 'Мясная', description: 'Лук, картофель, помидоры, грибы, сыр, оливки, мясо...', sizes: [22, 28, 33], price: 400},
+        {id: 4, preview: './assets/img/menu4.png', categories: ['Вегетарианская'], label: 'Сырная', description: 'Лук, картофель, помидоры, грибы, сыр, оливки, мясо...', sizes: [22, 28, 33], price: 330},
     ];
+
+    const parsedCategories = new Set(menu.map((item) => item.categories).flat());
+
+    const filterParent = document.querySelector('.menu-filter');
+
+    const generateFilter = () => {
+        const content = [...parsedCategories].map((item) => {
+            return `<div class="menu-filter__item" data-filter-value="${item}">${item}</div>`
+        })
+
+        filterParent.insertAdjacentHTML('beforeend', content.join(''))
+    };
+
+    if(parsedCategories && parsedCategories.size){
+        generateFilter();
+    }else{
+        filterParent.innerHTML = '';
+    };
 
     const middleIndex = Math.ceil(menu.length / 2);
 
@@ -26,8 +52,18 @@ window.addEventListener('DOMContentLoaded', () => {
     const menuListFirst = document.querySelector('.menu-list.first-part');
     const menuListSecond = document.querySelector('.menu-list.second-part');
 
-    const generateMenuList = (contentArr, node) => {
-        const content = contentArr.map((item) => {
+    const generateMenuList = (contentArr, node, filter = null) => {
+        node.innerHTML = '';
+
+        let filteredContent = contentArr;
+
+        if(filter){
+            filteredContent = contentArr.filter((item) => {
+                return item.categories.includes(filter)
+            })
+        };
+
+        const content = filteredContent.map((item) => {
             return `
             <li class="menu-list__item" data-id="${item.id}" itemscope itemprop="itemListElement" itemtype="http://schema.org/Product">
                 <img itemprop="image" loading="lazy" src="${item.preview}" alt="${item.label}" width="160" height="157" class="menu-list__item__preview">
@@ -61,4 +97,13 @@ window.addEventListener('DOMContentLoaded', () => {
 
     generateMenuList(firstPart, menuListFirst);
     generateMenuList(secondPart, menuListSecond);
+
+    window.addEventListener('click', ({target}) => {
+        if(target.className === 'menu-filter__item'){
+            filterItemClickHandler(target);
+
+            generateMenuList(firstPart, menuListFirst, target.dataset.filterValue);
+            generateMenuList(secondPart, menuListSecond, target.dataset.filterValue);
+        }
+    });
 });
